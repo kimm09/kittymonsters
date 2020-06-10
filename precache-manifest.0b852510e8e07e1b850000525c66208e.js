83ac95d9bd358f0c056a1b06e73b2c6725f21ab4
Kim <goverkim@msn.com>
2020-06-10 17:56:18 -0400

    Updates

---
+++ precache-manifest.0b852510e8e07e1b850000525c66208e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57eea30aac6f8d9ff6306185143b4aae",
+    "url": "/kittymonsters/index.html"
+  },
+  {
+    "revision": "11c2d704b42b8250bc8e",
+    "url": "/kittymonsters/static/css/main.8ad1fbc6.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/kittymonsters/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittymonsters/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c2d704b42b8250bc8e",
+    "url": "/kittymonsters/static/js/main.1f663fc3.chunk.js"
+  },
+  {
+    "revision": "52e0ab11351d515075e5",
+    "url": "/kittymonsters/static/js/runtime-main.fed12c5a.js"
+  }
+]);
